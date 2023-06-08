a7b18c9a9374ac78d89151325029c7e32dcffd78
TomashevskiiVladimir <vladimir.tomashevskii@tekschool.us>
2023-06-08 17:56:36 -0400

    6

---
+++ src/main/java/Interfaces/IDao.java
@@ -0,0 +1,12 @@
+package Interfaces;
+import java.util.List;
+
+public interface IDao<T, ID>  {
+
+    void create(T entity);
+    T read(ID id);
+    void update(T entity);
+    void delete(ID id);
+    List<T> getAll();
+
+}
