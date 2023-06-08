080ca4d8c4552b22b534a9a764d42bdf23ad8452
TomashevskiiVladimir <vladimir.tomashevskii@tekschool.us>
2023-06-08 17:38:36 -0400

    5

---
+++ src/main/java/model/EmployeesTasks.java
@@ -0,0 +1,22 @@
+package model;
+
+public class EmployeesTasks {
+    private long id;
+    private int percentageCompleted;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getPercentageCompleted() {
+        return percentageCompleted;
+    }
+
+    public void setPercentageCompleted(int percentageCompleted) {
+        this.percentageCompleted = percentageCompleted;
+    }
+}
---
+++ src/main/java/model/Tasks.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Tasks {
+    private long id;
+    private String name;
+    private String priority;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
