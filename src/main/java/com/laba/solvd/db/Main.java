73f5aa257815770b92eb42a291ca2825c8e89d01
TomashevskiiVladimir <vladimir.tomashevskii@tekschool.us>
2023-06-14 16:54:33 -0400

    add43

--- src/main/java/com/laba/solvd/db/Main.java
+++ src/main/java/com/laba/solvd/db/Main.java
@@ -1,8 +1,30 @@
 package com.laba.solvd.db;
+import com.laba.solvd.db.dao.CredentialDAO;
+import com.laba.solvd.db.dao.EmployeeDAO;
+import com.laba.solvd.db.service.CredentialsServiceImpl;
+import com.laba.solvd.db.service.EmployeesServiceImpl;
+import com.laba.solvd.db.model.Employee;
+import com.laba.solvd.db.model.Credential;
 
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        CredentialDAO credentialDAO = new CredentialDAO();
+        EmployeeDAO employeeDAO = new EmployeeDAO();
 
+        CredentialsServiceImpl credentialsService = new CredentialsServiceImpl(credentialDAO);
+        EmployeesServiceImpl employeesService = new EmployeesServiceImpl(employeeDAO);
+
+        Long credentialId = 1L;
+        Credential credential = credentialsService.read(credentialId);
+        System.out.println("Credential: " + credential);
+
+        List<Employee> employees = employeesService.findAll();
+        System.out.println("Employees: " + employees);
+
+        credentialDAO.close();
+        employeeDAO.close();
     }
 }
+
--- src/main/java/com/laba/solvd/db/service/EmployeesServiceImpl.java
+++ src/main/java/com/laba/solvd/db/service/EmployeesServiceImpl.java
@@ -1,22 +1,12 @@
 package com.laba.solvd.db.service;
 
 import com.laba.solvd.db.dao.EmployeeDAO;
-import com.laba.solvd.db.dao.Interfaces.IDAOCredential;
-import com.laba.solvd.db.dao.Interfaces.IDao;
-import com.laba.solvd.db.model.Skill;
-import com.laba.solvd.db.service.Interfaces.CredentialsRepository;
+
+
 import com.laba.solvd.db.service.Interfaces.EmployeesRepository;
-import com.laba.solvd.db.dao.ConnectionPool;
 import com.laba.solvd.db.model.Employee;
-import jdk.jfr.internal.Repository;
-
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-
-import java.util.List;
 
 public class EmployeesServiceImpl implements EmployeesRepository {
     private EmployeeDAO employeeDAO;
